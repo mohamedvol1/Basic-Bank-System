46337bb4fba86ab29517a0e7688ab4f7720b2af9
mohamedvol1 <70990780+mohamedvol1@users.noreply.github.com>
2022-02-21 19:24:24 +0200

    modfigy app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const customerRouter = require('./routes/customers/customer.router');
 
 const app = express();
-
-const customerRouter = require('./routes/customers/customer.router');
 
 // {
 // 		origin: ['http://localhost:3000', 'http://ez-bank.herokuapp.com']
 // 	}
 
-app.use(
-	cors()
-);
+app.use(cors());
 app.use(express.json());
-app.use(customerRouter);
 
 // app.use(express.static(path.join(__dirname, '..', '..', 'client/build')));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '..', '..', 'client/build')));
 }
 
-app.get("*", (req, res) => {
-	console.log('no route')
-	return res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'))
-})
+app.use(customerRouter);
 
+app.get('*', (req, res) => {
+	console.log('no route');
+	return res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'));
+});
 
 module.exports = app;
